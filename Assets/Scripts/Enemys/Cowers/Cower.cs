fc94af1f58672ea393b82b55baffc0558f5fc533
Alexander Mosyagin <sashamos1510@gmail.com>
2023-03-31 07:22:35 +0300

    Camera tilt speed

--- Assets/Scripts/Enemys/Cowers/Cower.cs
+++ Assets/Scripts/Enemys/Cowers/Cower.cs
@@ -4,37 +4,15 @@ namespace Enemys.Cowers
 {
     public class Cower : MonoBehaviour
     {
+        [SerializeField] private Transform[] _sides;
+        
         private Transform _transform;
-        private Collider _collider;
-
-        private Vector3 _leftBackwardCorner;
-        private Vector3 _rightBackwardCorner;
-        private Vector3 _leftForwardCorner;
-        private Vector3 _rightForwardCorner;
-
-        private Vector3 _leftSide;
-        private Vector3 _rightSide;
-        private Vector3 _backSide;
-        private Vector3 _frontSide;
 
         public Transform Transform => _transform;
 
         private void Awake()
         {
             _transform = transform;
-            _collider = GetComponent<Collider>();
-
-            _leftBackwardCorner = _collider.bounds.min;
-            _leftBackwardCorner.y = _transform.position.y;
-            _rightForwardCorner = _collider.bounds.max;
-            _rightForwardCorner.y = _transform.position.y;
-            _leftForwardCorner = new Vector3(_leftBackwardCorner.x, _transform.position.y, _rightForwardCorner.z);
-            _rightBackwardCorner = new Vector3(_rightForwardCorner.x, _transform.position.y, _leftBackwardCorner.z);
-
-            _leftSide = Vector3.Lerp(_leftBackwardCorner, _leftForwardCorner, 0.5f);
-            _rightSide = Vector3.Lerp(_rightBackwardCorner, _rightForwardCorner, 0.5f);
-            _backSide = Vector3.Lerp(_leftBackwardCorner, _rightBackwardCorner, 0.5f);
-            _frontSide = Vector3.Lerp(_leftForwardCorner, _rightForwardCorner, 0.5f);
         }
 
         public Vector3 GetCowerPoint(Vector3 position)
@@ -45,18 +23,13 @@ namespace Enemys.Cowers
 
         private Vector3 GetSide(Vector3 position)
         {
-            Vector3 side = _leftSide;
-            float distanceNew = Vector3.Distance(position, _rightSide);
-            if (distanceNew < Vector3.Distance(position, side))
-                side = _rightSide;
-            distanceNew = Vector3.Distance(position, _frontSide);
-            if (distanceNew < Vector3.Distance(position, side))
-                side = _frontSide;
-            distanceNew = Vector3.Distance(position, _backSide);
-            if (distanceNew < Vector3.Distance(position, side))
-                side = _backSide;
-
-            return side;
+            Vector3 result = _sides[0].position;
+            foreach (Transform side in _sides)
+            {
+                if (Vector3.Distance(position, side.position) < (Vector3.Distance(position, result)))
+                    result = side.position;
+            }
+            return result;
         }
     }
 }
--- Assets/Scripts/PlayerController/PlayerCamera.cs
+++ Assets/Scripts/PlayerController/PlayerCamera.cs
@@ -11,11 +11,14 @@ namespace  PlayerController
         [SerializeField] private Transform _origin;
         [SerializeField] private float _minYAngle, _maxYAngle;
         [SerializeField] private float _tiltHeadAngle;
+        [SerializeField] private float _tiltHeadSpeed;
 
         private Transform _transform;
         private Camera _camera;
         private float _rotationX,_rotationY;
         private float _defualtFieldOfView;
+        private float _headTargetAngle;
+        private float _headCurAngle;
 
         public Transform Transform => _transform;
 
@@ -28,6 +31,8 @@ namespace  PlayerController
             _camera = Camera.main;
             _defualtFieldOfView = _camera.fieldOfView;
             _sniperAim.enabled = false;
+            _headTargetAngle = 0;
+            _headCurAngle = 0;
         }
 
         private void Update()
@@ -35,22 +40,37 @@ namespace  PlayerController
             _transform.position = _origin.position;
         }
 
+        private void FixedUpdate()
+        {
+            if (Mathf.Abs(_headTargetAngle - _headCurAngle) > 0.1f)
+            {
+                _headCurAngle += _tiltHeadSpeed * Time.fixedDeltaTime * Mathf.Sign(_headTargetAngle - _headCurAngle);
+                _transform.rotation = Quaternion.Euler(_rotationX, _rotationY, _headCurAngle);
+                Debug.Log(_headCurAngle);
+            }
+            else
+            {
+                _transform.rotation = Quaternion.Euler(_rotationX, _rotationY, _headTargetAngle);
+                _headCurAngle = _headTargetAngle;
+            }
+        }
+
         public void Rotate(float angleX, float angleY)
         {
             _rotationY += angleX;
             _rotationX -= angleY;
             _rotationX = Mathf.Clamp(_rotationX, _minYAngle, _maxYAngle);
-            _transform.rotation = Quaternion.Euler(_rotationX, _rotationY, _transform.rotation.z);
+            _transform.rotation = Quaternion.Euler(_rotationX, _rotationY, _headCurAngle);
         }
 
         public void TiltHead(float dirX, float dirZ)
         {
             if (dirX == 0 || dirZ != 0)
-                _transform.rotation = Quaternion.Euler(_rotationX, _rotationY, 0);
+                _headTargetAngle = 0;
             else if (dirX < 0)
-                _transform.rotation = Quaternion.Euler(_rotationX, _rotationY, _tiltHeadAngle);
+                _headTargetAngle = _tiltHeadAngle;
             else
-                _transform.rotation = Quaternion.Euler(_rotationX, _rotationY, -_tiltHeadAngle);
+                _headTargetAngle = -_tiltHeadAngle;
         }
 
         public void Zoom(float value, bool state)
